96ed989e1742535cc8de7e0e2b991cd2bbe33a47
Mark Schultz <schultzm8@gmail.com>
2013-08-05 18:13:35 -0500

    initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InTheHand.Net.Sockets;
+using InTheHand.Net;
+using InTheHand.Net.Bluetooth;
+using InTheHand.Net.Bluetooth.AttributeIds;
+using Brecham.Obex;
+using Brecham.Obex.Objects;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var address = "04FE3167133D";
+            BluetoothAddress addr = BluetoothAddress.Parse(address);
+            var endpoint = new BluetoothEndPoint(addr, BluetoothService.MessageAccessProfile, 16);
+            var bdi = new BluetoothDeviceInfo(addr);
+            var records = bdi.GetServiceRecords(BluetoothService.MessageAccessProfile);
+            foreach (ServiceRecord record in records)
+            {
+                var port = ServiceRecordHelper.GetRfcommChannelNumber(record);
+                var name = record.GetPrimaryMultiLanguageStringAttributeById(UniversalAttributeId.ServiceName);
+                Console.WriteLine("Name: " + name + "; Port: " + port);
+            }
+            var cli = new BluetoothClient();
+            cli.Encrypt = true;
+            cli.Connect(endpoint);
+            var session = new ObexClientSession(cli.GetStream(), 65535);
+            //session.Connect(ObexConstant.Target.FolderBrowsing);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ btSMS/Program.fs
@@ -0,0 +1,45 @@
+﻿open System
+open System.Windows
+open System.Linq
+open System.Windows.Controls
+open InTheHand.Windows.Forms
+open InTheHand.Net.Sockets
+open InTheHand.Net
+open InTheHand.Net.Bluetooth
+open Brecham.Obex
+open Brecham.Obex.Objects
+
+type MainWindow = FSharpx.XAML<"Window.xaml">
+
+let bluetoothtest ep =
+    let client = new BluetoothClient()
+    try
+        client.Connect(ep)
+        let session = new ObexClientSession(client.GetStream(), 65535)
+        session.Connect(ObexConstant.Target.FolderBrowsing)
+        session.Get("1","x-bt/message")
+        let listing = session.GetFolderListing()
+        if listing.Files.Count > 0 then
+            listing.Files.FirstOrDefault().Name
+        else
+            ""
+    finally
+        ()
+
+let loadWindow() =
+    let window = MainWindow()
+    window.Button1.Click.Add(fun _ -> 
+        let dlg = new SelectBluetoothDeviceDialog()
+        let result = dlg.ShowDialog()
+        let device = dlg.SelectedDevice
+        let addr = device.DeviceAddress
+        let endpoint = new BluetoothEndPoint(addr, BluetoothService.RFCommProtocol)
+        //Array.fold (fun (i,j) -> ServiceRecordHelper.GetRfcommChannelNumber(j) |> (+) i) () (device.GetServiceRecords(BluetoothService.RFCommProtocol)) |> ignore
+        
+        window.Textbox1.Text = device.InstalledServices.ToString() |> ignore )
+        //window.Textbox1.Text = bluetoothtest endpoint |> ignore )
+    window.Root
+ 
+[<STAThread>]
+(new Application()).Run(loadWindow()) 
+|> ignore
