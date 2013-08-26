f707440d55cf5cf847a37e70bcc9fe1439cae754
Mark Schultz <schultzm8@gmail.com>
2013-08-26 18:00:04 -0500

    remove old f#

--- btSMS/Program.fs
+++ btSMS/Program.fs
@@ -6,25 +6,11 @@ open InTheHand.Windows.Forms
 open InTheHand.Net.Sockets
 open InTheHand.Net
 open InTheHand.Net.Bluetooth
+open InTheHand.Net.Bluetooth.AttributeIds
 open Brecham.Obex
 open Brecham.Obex.Objects
 
 type MainWindow = FSharpx.XAML<"Window.xaml">
-
-let bluetoothtest ep =
-    let client = new BluetoothClient()
-    try
-        client.Connect(ep)
-        let session = new ObexClientSession(client.GetStream(), 65535)
-        session.Connect(ObexConstant.Target.FolderBrowsing)
-        session.Get("1","x-bt/message")
-        let listing = session.GetFolderListing()
-        if listing.Files.Count > 0 then
-            listing.Files.FirstOrDefault().Name
-        else
-            ""
-    finally
-        ()
 
 let loadWindow() =
     let window = MainWindow()
