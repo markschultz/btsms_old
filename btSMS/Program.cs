ca17db7e87a5df052f63cb22a03eccf42b443d9d
Mark Schultz <schultzm8@gmail.com>
2013-09-03 11:11:58 -0500

    change namespace. use 'using' for idisposables

--- btSMS/Program.cs
+++ btSMS/Program.cs
@@ -10,7 +10,7 @@ using InTheHand.Net.Bluetooth.AttributeIds;
 using Brecham.Obex;
 using Brecham.Obex.Objects;
 
-namespace ConsoleApplication1
+namespace btSMS
 {
     class Program
     {
@@ -28,41 +28,41 @@ namespace ConsoleApplication1
                 var name = record.GetPrimaryMultiLanguageStringAttributeById(UniversalAttributeId.ServiceName);
                 Console.WriteLine("Name: " + name + "; Port: " + port);
             }
-            var cli = new BluetoothClient();
-            cli.Encrypt = true;
-            cli.Connect(endpoint);
-            Console.WriteLine("Connected: " + cli.Connected + " Port: " + cli.RemoteEndPoint.Port);
-            var session = new ObexClientSession(cli.GetStream(), 65535);
-            byte[] masUUID = { 0xBB, 0x58, 0x2B, 0x40, 0x42, 0x0C, 0x11, 0xDB, 0xB0, 0xDE, 0x08,
-                             0x00, 0x20, 0x0C, 0x9A, 0x66 };
-            byte[] mnsUUID = { 0xBB, 0x58, 0x2B, 0x41, 0x42, 0x0C, 0x11, 0xDB, 0xB0, 0xDE, 0x08,
-                             0x00, 0x20, 0x0C, 0x9A, 0x66 };
-            try
+            using (var cli = new BluetoothClient())
             {
-                session.Connect(masUUID);
-                Console.WriteLine("Session Connected: " + session.ConnectionId);
-                Console.WriteLine(session.GetFolderListing().AllItems.Aggregate("Current Folder Listing:\n\t", (q, a) => q + a.ToString() + "\n\t"));
-                session.SetPath("telecom");
-                Console.WriteLine(session.GetFolderListing().AllItems.Aggregate("Current Folder Listing:\n\t", (q, a) => q + a.ToString() + "\n\t"));
-                session.SetPath("msg");
-                Console.WriteLine(session.GetFolderListing().AllItems.Aggregate("Current Folder Listing:\n\t", (q, a) => q + a.ToString() + "\n\t"));
-                byte[] ba = new byte[1024 * 4];
-                //GetSingle(session, ba);
-                //GetListing(session, ba);
-                SendMessage(session, ba, "test test", "3124361855");
-            }
-            catch (ObexResponseException obexRspEx)
-            {
-                if (obexRspEx.ResponseCode == Brecham.Obex.Pdus.ObexResponseCode.PeerUnsupportedService)
+                cli.Encrypt = true;
+                cli.Connect(endpoint);
+                Console.WriteLine("Connected: " + cli.Connected + " Port: " + cli.RemoteEndPoint.Port);
+                using (var session = new ObexClientSession(cli.GetStream(), 65535))
                 {
-                    Console.WriteLine("The OBEX Server does not support the requested Target service/application.");
-                }
-                else
-                {
-                    Console.WriteLine("The OBEX Server rejected our connection: " + obexRspEx.ResponseCode);
-                    if (obexRspEx.Description != null)
+                    try
                     {
-                        Console.WriteLine("    Reason: " + obexRspEx.Description);
+                        session.Connect(Constants.masUUID);
+                        Console.WriteLine("Session Connected: " + session.ConnectionId);
+                        Console.WriteLine(session.GetFolderListing().AllItems.Aggregate("Current Folder Listing:\n\t", (q, a) => q + a.ToString() + "\n\t"));
+                        session.SetPath("telecom");
+                        Console.WriteLine(session.GetFolderListing().AllItems.Aggregate("Current Folder Listing:\n\t", (q, a) => q + a.ToString() + "\n\t"));
+                        session.SetPath("msg");
+                        Console.WriteLine(session.GetFolderListing().AllItems.Aggregate("Current Folder Listing:\n\t", (q, a) => q + a.ToString() + "\n\t"));
+                        byte[] ba = new byte[1024 * 4];
+                        //GetSingle(session, ba);
+                        //GetListing(session, ba);
+                        SendMessage(session, ba, "test test", "3124361855");
+                    }
+                    catch (ObexResponseException obexRspEx)
+                    {
+                        if (obexRspEx.ResponseCode == Brecham.Obex.Pdus.ObexResponseCode.PeerUnsupportedService)
+                        {
+                            Console.WriteLine("The OBEX Server does not support the requested Target service/application.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("The OBEX Server rejected our connection: " + obexRspEx.ResponseCode);
+                            if (obexRspEx.Description != null)
+                            {
+                                Console.WriteLine("    Reason: " + obexRspEx.Description);
+                            }
+                        }
                     }
                 }
             }
@@ -80,7 +80,7 @@ namespace ConsoleApplication1
             headers.Dump(Console.Out);
             UTF8Encoding utf = new UTF8Encoding();
             var put = session.Put(headers);
-            ba = utf.GetBytes(Strings.sendTemplate.Replace("%message", message).Replace("%toNumber", to));
+            ba = utf.GetBytes(Constants.sendTemplate.Replace("%message", message).Replace("%toNumber", to));
             put.Write(ba, 0, ba.Length);
             put.Close();
         }
--- btSMS/Properties/AssemblyInfo.cs
+++ btSMS/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("btSMS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("RPC-SCCM")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("btSMS")]
 [assembly: AssemblyCopyright("Copyright © RPC-SCCM 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
